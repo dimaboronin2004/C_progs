dc0361b91cd787fd54b9b8e67de233ae2cb6785b
dimaboronin2004 <112697344+dimaboronin2004@users.noreply.github.com>
2023-11-27 01:28:23 +0300

    Update insertionSort.h

--- insertionSort.h
+++ insertionSort.h
@@ -1,5 +1,5 @@
 #ifndef UNTITLED_INSERTIONSORT_H
 #define UNTITLED_INSERTIONSORT_H
 #include <stdint.h>
-extern void insertionSort(int len, int arr[]);
+extern void insertionSort(int *len, int *arr);
 #endif //UNTITLED_INSERTIONSORT_H
